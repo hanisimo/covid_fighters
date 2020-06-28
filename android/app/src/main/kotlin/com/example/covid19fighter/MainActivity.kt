3a0ab6d1e7fcc3b9aa57435e39273668ad2f2560
Han <hanisimo@gmail.com>
2020-06-28 21:32:45 +0400

    2020-06-28 V0.6 Commit

---
+++ android/app/src/main/kotlin/com/example/covid19fighter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.covid19fighter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/hanisimo/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/hanisimo/Desktop/FP/covid_fighters"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_APPLICATION_PATH=/Users/hanisimo/Desktop/FP/covid_fighter/covid_fighter"
+export "FLUTTER_TARGET=/Users/hanisimo/Desktop/FP/covid_fighter/covid_fighter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_FRAMEWORK_DIR=/Users/hanisimo/Development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
