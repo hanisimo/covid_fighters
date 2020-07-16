ec3779f1857e3765b98252e22dac1c973d463cdc
Han <hanisimo@gmail.com>
2020-07-16 12:51:10 +0400

    2020-07-16 V0.7.2: - Add Launcher Icon (iOS & Android). - Add Launcher Name. - Add Native Splash. - Move the game graphics to a new "graphics" folder within the App assets.

--- android/app/src/main/kotlin/com/example/covid19fighter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/covid19fighter/MainActivity.kt
@@ -2,5 +2,8 @@ package com.example.covid19fighter
 
 import io.flutter.embedding.android.FlutterActivity
 
+import android.os.Build
+import android.view.ViewTreeObserver
+import android.view.WindowManager
 class MainActivity: FlutterActivity() {
 }
--- lib/engine/utils.dart
+++ lib/engine/utils.dart
@@ -4,7 +4,7 @@ import 'package:flutter/services.dart';
 
 // Load Graphics Assets
 Future<ui.Image> loadGraphicsAssets() async {
-  ByteData graphicsAssets = await rootBundle.load("assets/graphics.png");
+  ByteData graphicsAssets = await rootBundle.load("assets/graphics/graphics.png");
   ui.Codec instantiateImageCodec =
   await ui.instantiateImageCodec(graphicsAssets.buffer.asUint8List());
   ui.FrameInfo frameInfo = await instantiateImageCodec.getNextFrame();
